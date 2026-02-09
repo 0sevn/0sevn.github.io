a1c66c18b5683ab4627f0b772dc3dd46a9921f85
0sevn <71658261+0sevn@users.noreply.github.com>
2026-02-09 12:04:46 +0100

    Update script.js

--- floweav3/script.js
+++ floweav3/script.js
@@ -116,7 +116,7 @@ requestAnimationFrame(() => {
                         // Your existing delete logic here
                         // purgeList();
                         // togglePurgeButton(); 
-                        scroll_div.scrollTo({ left: 100, behavior: 'instant' });
+                        scroll_div.scrollTo({ left: 99, behavior: 'instant' });
                         purgeSpecificTask(taskId);
                         // saveData();
                     }, 100); // 1 second delay
